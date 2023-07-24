f989f90e6c9f1d5e6d80c2bed6d603df89122f3b
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-07-24 16:36:35 +0700

    realtimeChat

--- index.js
+++ index.js
@@ -11,26 +11,27 @@ ioSocket.on("connection", (socket) => {
     socket.on("add-user-new", (newUser) => {
         if (!userConnet.some((user) => user.idUser === newUser)) {
             userConnet.push({idUser: newUser, socketId: socket.id });
-            console.log("new user add : ", userConnet)
+            // console.log("new user add =================== : ", userConnet)
         }
-        ioSocket.emit("get-users", userConnet)
-    })
-
-    socket.on("send-message", (message) => {
-        const {idUserReceive} = message
-        const userReceive = userConnet.find((user) => user.idUser === idUserReceive)
-        console.log("Send from socket to", idUserReceive, "data send : ", message)
-        
-        if (userReceive) {
-            ioSocket.to(userReceive.socketId).emit("message", message)
-        }
-
+        ioSocket.emit("get-users", userConnet)  
     })
 
     socket.on("disconnect", () => {
         userConnet = userConnet.filter((user) => user.socketId !== socket.id)
-        console.log("user after disconnected: ", userConnet)
         ioSocket.emit("get-user", userConnet)
     })
+
+    socket.on("send-message", (message) => {
+        // console.log('mesageeeeeeeeeeee: ', message)
+        const {idUserReceive} = message
+
+        const userReceive = userConnet.find((user) => user.idUser === idUserReceive) // tìm thiết bị đang hoạt động
+        console.log("gửi đến id", idUserReceive, "dữ liệu gửi : ", message, 'thông tin nhận: ', userReceive)
+        if (userReceive) {
+            ioSocket.to(userReceive.socketId).emit("recevied-message", message)
+        }
+    })
+
+    
 
 })
